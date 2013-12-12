751a4a43c8a0c1c18f6356bb0932539429fa1ab2
aadaa111 <aadaa111@yahoo.com>
2013-12-11 19:01:41 -0800

    Update SizerHomeFollows.js

--- SizerHomeFollows.js
+++ SizerHomeFollows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-    var defaultKey = '2APIXmLASYLxiQf1VRXuSGZ7tWfvfKaDOTq7BWyS6DPw477n', // Unique master Xively API key to be used as a default
-        defaultFeeds = [439949384], // Comma separated array of Xively Feed ID numbers
+    var defaultKey = '5pZGok5BY4sIWrRiz77FgScAT743WVVfyUU9aPqK3V8s2ro6', // Unique master Xively API key to be used as a default
+        defaultFeeds = [1884847396], // Comma separated array of Xively Feed ID numbers
         applicationName = 'SizerWebDemo', // Replaces Xively logo in the header
         dataDuration = '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
         dataInterval = 10, // Default interval for data to be displayed (in seconds)
